1050dabb189dc8da3ed9f6a7aa73f682836fe62e
Michael Lesniak <mlesniak@micromata.de>
2018-06-22 22:17:02 +0200

    Updates

--- life/main.js
+++ life/main.js
@@ -1,12 +1,11 @@
-// TODO ML Optimize createGrid()
-
 let fs = false;
 let size = 10;
 
 let cols;
 let rows;
 
-let grid = undefined;
+let grids = [];
+let index = 0;
 
 function setup() {
     let c = createCanvas(windowWidth, windowHeight);
@@ -16,65 +15,75 @@ function setup() {
     cols = floor(windowWidth / size);
     rows = floor(windowHeight / size);
     
-    grid = createGrid(cols, rows);
+    index = 0;
+    grids[0] = createGrid(cols, rows);
+    grids[1] = createGrid(cols, rows);
     for (let x = 0; x < cols; x++) {
         for (let y = 0; y < rows; y++) {
-            grid[x][y] = floor(random(2));
+            grids[0][x][y] = floor(random(2));
+            grids[1][x][y] = grids[0][x][y];
         }
     }
 }
 
 function draw() {
     drawGrid();
+    fill(255, 0, 0);
+    // For debugging index.
+    // text(index, 0, 10);
     if (!mouseIsPressed) {
-        grid = updateGrid(grid);
+        updateGrid();
     }
+    
 }
 
 function drawGrid() {
-    background(255);
+    let currentGrid = grids[index % 2];
+    background(0);
     for (let x = 0; x < cols; x++) {
         for (let y = 0; y < rows; y++) {
-            if (grid[x][y] == 1) {
-                stroke(200);
-                fill(0);
+            if (currentGrid[x][y] == 1) {
+                stroke(0);
+                fill(0, 200, 0);
             } else {
                 if (size < 10) {
                     noStroke();
                 }
-                fill(255);
+                fill(0);
             }
             rect(x * size, y * size, size - 1, size - 1);
         }
     }
 }
 
-function updateGrid(grid) {
-    let ng = createGrid(cols, rows);
-    
+function updateGrid() {
+    let currentGrid = grids[index % 2];
+    let nextGrid = grids[(index + 1) % 2];
     for (let x = 0; x < cols; x++) {
         for (let y = 0; y < rows; y++) {
-            let n = countNeighbours(grid, x, y);
-            if (grid[x][y] == 0 && n == 3) {
-                ng[x][y] = 1;
+            let n = countNeighbours(currentGrid, x, y);
+            let value = currentGrid[x][y];
+            nextGrid[x][y] = currentGrid[x][y];
+            if (value == 0 && n == 3) {
+                nextGrid[x][y] = 1;
             }
-            if (grid[x][y] == 1 && n < 2) {
-                ng[x][y] = 0;
+            if (value == 1 && n < 2) {
+                nextGrid[x][y] = 0;
             }
-            if (grid[x][y] == 1 && (n == 2 || n == 3)) {
-                ng[x][y] = 1;
+            if (value == 1 && (n == 2 || n == 3)) {
+                nextGrid[x][y] = 1;
             }
-            if (grid[x][y] == 1 && n > 3) {
-                ng[x][y] = 0;
+            if (value == 1 && n > 3) {
+                nextGrid[x][y] = 0;
             }
         }
     }
-    
-    return ng;
+
+    index++;
 }
 
 function mouseDragged() {
-    grid[floor(mouseX / size)][floor(mouseY / size)] = 1;
+    grids[index % 2][floor(mouseX / size)][floor(mouseY / size)] = 1;
 }
 
 function countNeighbours(grid, x, y) {
@@ -108,6 +117,10 @@ function createGrid(cols, rows) {
     return g;
 }
 
+function mouseClicked() {
+    updateGrid();
+}
+
 /**
  * Fullscreen handling.
  */
@@ -115,6 +128,24 @@ function keyPressed() {
     if (key === 'F') {
         fs = !fs;
         fullscreen(fs);
+        setup();
+    }
+
+    if (key === 'R') {
+        setup();
+    }
+
+    console.log(key);
+    if (key === 'K') {
+        size += 2;
+        setup();
+    }
+
+    if (key === 'J') {
+        if (size == 4) {
+            return;
+        }
+        size -= 2;
         setup();
     }
 }

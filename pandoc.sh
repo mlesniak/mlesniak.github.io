5aae4a295373ba06778374d569e3ac9b08b5f5ba
Michael Lesniak <mail@mlesniak.com>
2023-08-14 21:44:45 +0200

    Mon Aug 14 21:44:45 CEST 2023

--- pandoc.sh
+++ pandoc.sh
@@ -8,5 +8,5 @@ do
     echo "Parsing $md"
     pandoc --quiet $md --css pandoc.css -s -o docs/$(echo $md|rev|cut -d. -f2|rev|cut -b2-).html
 done
-cp pandoc.css docs
+cp pandoc.css CNAME docs
 git add . && git commit -m"$(date)" && git push

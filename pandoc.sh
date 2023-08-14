6ee9931ccb8df29ceff94fc5a2c16ac113dfea9e
Michael Lesniak <mail@mlesniak.com>
2023-08-14 20:38:42 +0200

    Mon Aug 14 20:38:42 CEST 2023

--- pandoc.sh
+++ pandoc.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env sh
 
+set -e
+set -x
+
+git checkout main
+git add . && git commit -m"$(date)" && git push
+
+git checkout gh-pages
 for md in $(find . -name \*md)
 do
     echo "Parsing $md"
     pandoc --quiet $md --css pandoc.css -s -o $(echo $md|rev|cut -d. -f2|rev|cut -b2-).html
 done
 
-echo "Pushing to GitHub"
-(git add . && git commit -m"$(date)" && git push) >/dev/null 2>&1
-(git checkout gh-pages && git merge main && git push && git checkout main) >/dev/null 2>&1
+echo "Deploying to GitHub"
+git merge main && git push && git checkout main

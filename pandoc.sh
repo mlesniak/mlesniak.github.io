58daf0315cfa5343c44bc729a184f81a6d389241
Michael Lesniak <mail@mlesniak.com>
2023-08-15 06:38:00 +0200

    Tue Aug 15 06:38:00 CEST 2023

--- pandoc.sh
+++ pandoc.sh
@@ -6,7 +6,7 @@ set -x
 for md in $(find . -name \*md)
 do
     echo "Parsing $md"
-    pandoc --quiet $md --css pandoc.css -s -o docs/$(echo $md|rev|cut -d. -f2|rev|cut -b2-).html
+    pandoc --quiet $md --css pandoc.css -B header.html -s -o docs/$(echo $md|rev|cut -d. -f2|rev|cut -b2-).html
 done
 cp pandoc.css CNAME .nojekyll docs
 git add . && git commit -m"$(date)" && git push

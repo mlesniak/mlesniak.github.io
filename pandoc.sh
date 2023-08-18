6c2f58d92008359d2ce47484bded3d0d8a1ae097
Michael Lesniak <michael.lesniak@scalable.capital>
2023-08-18 13:26:46 +0200

    Update

--- pandoc.sh
+++ pandoc.sh
@@ -6,11 +6,11 @@ set -x
 for md in $(find . -name \*md)
 do
     echo "Parsing $md"
-    pandoc --quiet $md --css pandoc.css --template template.html -B header.html -s -o docs/$(echo $md|rev|cut -d. -f2|rev|cut -b2-).html
+    pandoc --quiet $md --css pandoc.css --template template.html -o docs/$(echo $md|rev|cut -d. -f2|rev|cut -b2-).html
 done
-cp pandoc.css CNAME .nojekyll *.png *.ico docs
+cp *.css CNAME .nojekyll *.png *.ico docs
 
 if [ -z "$1" ]
 then
-    git add . && git commit -m"$(date)" && git push
+    git add . && git commit -m"Update" && git push
 fi
